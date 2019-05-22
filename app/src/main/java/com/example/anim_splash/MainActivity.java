23fe4437a1ce1e22d1dfb81b60b9ad507f87e7b4
ljr <15622732935@163.com>
2019-05-22 17:40:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/anim_splash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.anim_splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//ViewPager
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
